54976ec574f151219514ba86e3f03481ac41525a
Son Nguyen <business@banhmi.dev>
2025-02-17 13:45:58 +0100

    Fix syntax

--- android/src/main/java/com/example/r_upgrade/RUpgradePlugin.java
+++ android/src/main/java/com/example/r_upgrade/RUpgradePlugin.java
@@ -16,7 +16,6 @@ import io.flutter.embedding.engine.plugins.activity.ActivityPluginBinding;
 import io.flutter.plugin.common.BinaryMessenger;
 import io.flutter.plugin.common.MethodChannel;
 import io.flutter.plugin.common.PluginRegistry;
-import io.flutter.plugin.common.PluginRegistry.Registrar;
 
 /**
  * RUpgradePlugin
